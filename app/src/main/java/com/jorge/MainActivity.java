9e186b3b63dba54af1f24b5cf11d2e280f00ce9d
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-05-22 18:55:22 -0600

    Fragmentos creados para diseñar

--- app/src/main/java/com/jorge/MainActivity.java
+++ app/src/main/java/com/jorge/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         boolean seleccionado = false;
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
